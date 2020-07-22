edb87b4c90316c44237d54aff67f81f094db1fc9
dshik <dshik@mail.ru>
2020-07-22 22:19:33 +0300

    Создан проект DZ02-Lesson02

---
+++ Lesson02/DZ02-Lesson02/DZ02-Lesson02/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  DZ02-Lesson02
+//
+//  Created by Дмитрий Шикунов on 22.07.2020.
+//  Copyright © 2020 Дмитрий Шикунов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+/* Функция которая определяет четное число или нет*/
