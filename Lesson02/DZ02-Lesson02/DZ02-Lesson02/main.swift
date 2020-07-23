a383e7e3a4c5c9c37b4513dbabecffec435adea2
dshik <dshik@mail.ru>
2020-07-23 12:42:34 +0300

    Сделано задание 1 второго урока

--- Lesson02/DZ02-Lesson02/DZ02-Lesson02/main.swift
+++ Lesson02/DZ02-Lesson02/DZ02-Lesson02/main.swift
@@ -12,3 +12,23 @@ print("Hello, World!")
 
 /* Задание 2.1*/
 /* Функция которая определяет четное число или нет*/
+
+func isEvenNumber(intNumber: Int) -> Bool {
+    return intNumber%2 == 0
+}
+
+/*Проверка*/
+let intNum1 = 10
+let intNum2 = 11
+
+if isEvenNumber(intNumber: intNum1) {
+    print("\(intNum1) - чётное число.")
+} else {
+    print("\(intNum1) - нечётное число.")
+}
+
+if isEvenNumber(intNumber: intNum2) {
+    print("\(intNum2) - чётное число.")
+} else {
+    print("\(intNum2) - нечётное число.")
+}
