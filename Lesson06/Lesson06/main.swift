8a8733d4ef773a6871ec04f310e54e656df1423b
dshik <dshik@mail.ru>
2020-08-07 12:06:00 +0300

    Начато задание к уроку 6

---
+++ Lesson06/Lesson06/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson06
+//
+//  Created by Дмитрий Шикунов on 07.08.2020.
+//  Copyright © 2020 Дмитрий Шикунов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
